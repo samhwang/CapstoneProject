ac32270a7651be9e94238b1c08a3f41fe4c594a3
Bin Lu <seanlu200@gmail.com>
2014-05-19 11:01:51 +1000

    added project

---
+++ src/POMining/src/com/po/main/PoMMain.java
@@ -0,0 +1,10 @@
+package com.po.main;
+
+public class PoMMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/POMining/src/com/po/rss/RSSClient.java
@@ -0,0 +1,5 @@
+package com.po.rss;
+
+public class RSSClient {
+
+}
