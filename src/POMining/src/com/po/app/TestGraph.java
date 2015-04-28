126881a79979a7e7ccde9c06a54e62780c5c8c1c
Sam Huynh <samhwang21121994@gmail.com>
2015-04-28 22:06:44 +1000

    re-importing lib-files and try a test chart with charts4j
    Stuart, remember to import the jar files into Eclipse as well as I
    don’t know if Eclipse will do it automatically. Right click on POMining
    => Properties => Java Build Path => Add JARs and add the jar files in
    there

---
+++ src/POMining/src/com/po/app/TestGraph.java
@@ -0,0 +1,18 @@
+package com.po.app;
+
+
+import static com.googlecode.charts4j.Color.*;
+import static com.googlecode.charts4j.UrlUtil.normalize;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TestGraph {
+
+}
